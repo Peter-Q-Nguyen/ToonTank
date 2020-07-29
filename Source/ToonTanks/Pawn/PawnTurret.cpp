50e93132526a716446e5c5f3a54adedbce4987dc
Peter Nguyen <spacechannel6@gmail.com>
2020-07-29 01:07:15 +0100

    Initial commit with PawnBase class, and two derived children, pawntank (player class with camera, spring arm, and input movement), and pawn turret (for an AI controlled turret). Currenty the pawn turret can determine if player tank is in range, but cannot do anything else

---
+++ Source/ToonTanks/Pawn/PawnTurret.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "PawnTank.h"
+
+APawnTurret::APawnTurret()
+{
+
+}
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+	PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void APawnTurret::CheckFireCondition()
+{
+	// If Player == null || is Dead then bail
+	if (!PlayerPawn)
+	{
+		return;
+	}
+
+	//if Player Is in range then fire
+	if (ReturnDistanceToPlayer() <= FireRange)
+	{
+		// Fire
+		UE_LOG(LogTemp, Warning, TEXT("Fire Condition Success"))
+	}
+}
+
+float APawnTurret::ReturnDistanceToPlayer()
+{
+	if (!PlayerPawn)
+	{
+		return 0.0f;
+	}
+
+	return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+}
