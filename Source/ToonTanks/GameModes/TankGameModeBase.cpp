204ae8e5a3005d3afcd2290de5ed9b785f2f3452
Peter Nguyen <spacechannel6@gmail.com>
2020-07-31 00:22:39 +0100

    add gameflow to gamemodebase, keeping track of winlose state, and handling Actor deaths.Move HandleDestruction to public (from private)

--- Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -2,32 +2,59 @@
 
 
 #include "TankGameModeBase.h"
+#include "ToonTanks/Pawn/PawnTank.h"
+#include "ToonTanks/Pawn/PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
 
 void ATankGameModeBase::BeginPlay()
 {
 	// get and store game win/lose conditions
+	Super::BeginPlay();
+
+
+	HandleGameStart();
 
 }
 
 void ATankGameModeBase::ActorDied(AActor* DeadActor)
 {
-	//check what actor died, if turret, tally, if player-> go to lose condition
-	UE_LOG(LogTemp, Warning, TEXT("A Pawn Died"));
+	if (DeadActor == PlayerTank)
+	{
+		PlayerTank->HandleDestruction();
+		HandleGameOver(false);
+	}
+	else if (APawnTurret * DestroyedTurret = Cast<APawnTurret>(DeadActor))
+	{
+		DestroyedTurret->HandleDestruction();
+
+		if (--TargetTurrets == 0)
+		{
+			HandleGameOver(true);
+		}
+	}
 }
 
+
+int32 ATankGameModeBase::GetTargetTurretCount()
+{
+	TArray<AActor*> TurretActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APawnTurret::StaticClass(), TurretActors);
+	return TurretActors.Num();
+}
 
 void ATankGameModeBase::HandleGameStart()
 {
 	//initialise start countdown, turret activation, pawn check
 	//call blueprint game start
+	GameStart();
 
 }
 
 void ATankGameModeBase::HandleGameOver(bool PlayerWon)
 {
-	// see if player killed all turrets and show win result
-	// otherwise show lose
-	// call blueprint game over.
+	TargetTurrets = GetTargetTurretCount();
+	PlayerTank = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+	GameOver(PlayerWon);
 }
 
 
--- Source/ToonTanks/Pawn/PawnBase.h
+++ Source/ToonTanks/Pawn/PawnBase.h
@@ -33,12 +33,14 @@ public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
+	virtual void HandleDestruction();
+
 protected:
 
 	void RotateTurret(FVector LookAtTarget);
 
 	void Fire();
 
-	virtual void HandleDestruction();
+
 
 };
